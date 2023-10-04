3104bfe7001013f8a5d2856cb125f2e3af7fdad4
victoria <xvyabo@ourFEDORA.hitronhub.home>
2023-10-04 14:01:59 -0700

    Updates

--- bash-scripts/all-customs.sh
+++ bash-scripts/all-customs.sh
@@ -14,10 +14,13 @@ sudo unzip kora-1-6-1.zip -d /usr/share/icons
 sudo dnf upgrade --refresh 
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
+# github repo
+sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+
 # flatpak - flathub repo
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.spotify.Client com.mattjakeman.ExtensionManager
 
 # dnf packages
-sudo dnf install -y fish gnome-tweaks discord steam VirtualBox akmod-nvidia xorg-x11-drv-nvidia
+sudo dnf install -y fish gnome-tweaks discord steam VirtualBox akmod-nvidia xorg-x11-drv-nvidia gh
 
--- bash-scripts/packages.sh
+++ bash-scripts/packages.sh
@@ -4,9 +4,12 @@
 sudo dnf upgrade --refresh 
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
+# github repo
+sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+
 # flatpak - flathub repo
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.spotify.Client com.mattjakeman.ExtensionManager com.github.Eloston.UngoogledChromium
 
 # dnf packages
-sudo dnf install -y fish gnome-tweaks discord steam VirtualBox akmod-nvidia xorg-x11-drv-nvidia
+sudo dnf install -y fish gnome-tweaks discord steam VirtualBox akmod-nvidia xorg-x11-drv-nvidia gh
